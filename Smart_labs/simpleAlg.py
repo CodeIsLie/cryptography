6f88889ad3a0643e5f358377f8eb00b763b658b1
Ilya <ilya1757@gmail.com>
2018-02-13 18:06:06 +0300

    legandr

--- Smart_labs/simpleAlg.py
+++ Smart_labs/simpleAlg.py
@@ -2,8 +2,14 @@
 Basic algebra algs
 """
 
+from math import ceil, floor, sqrt
+from functools import reduce
+
 def partitions(iter1, iter2):
     return [(x, y) for x in iter1 for y in iter2]
+
+def product(iter):
+    return reduce(lambda x, y: x* y, iter, 1)
 
 def euclid_GCD(a, b):
     def euclid_step(x, y):
@@ -24,5 +30,37 @@ def euclid_GCD(a, b):
         gcd *= 2
 
     return gcd * euclid_step(a, b)
+
+def get_dividers(x):
+    dividers = []
+    for i in range(2, round(sqrt(x)) + 1):
+        degree = 0
+        while x % i == 0:
+            degree += 1
+            x /= i
+        if degree % 2 == 1:
+            dividers.append(i)
+        if x == 1:
+            break
+    return dividers
+
+def legandr(a, p):
+    if a == 1:
+        return 1
+    if a == 2:
+        return 1 if (p % 8) in (1, 7) else -1
+    #if a == -1:
+    #    return (2)
+    if a > p:
+        return legandr(a % p, p)
+
+    dividers = get_dividers(a)
+    if len(dividers) == 1:
+        return legandr(p, a) * -1 if p % 4 == 3 and a % 4 == 3 else 1
+    return product([legandr(div, p) for div in dividers])
+
+
+def extended_euclid(a, n):
+    return None
 
 
--- Smart_labs/tests.py
+++ Smart_labs/tests.py
@@ -3,6 +3,14 @@ from simpleAlg import *
 
 narr1 = [1, 2, 3, 5, 7, 9]
 narr2 = [2, 4, 6, 8, 10]
-
+"""
 for x, y in partitions(narr1, narr2):
     print("GCD({1}, {2}) = {0}".format(euclid_GCD(x, y), x, y))
+
+"""
+
+leg1 = [1, 2, 3, 4, 5, 6, 7]
+leg2 = [3, 5, 7, 11, 13, 19]
+
+for y, x in partitions(leg2, leg1):
+    print("({1}/{2}) = {0}".format(legandr(x, y), x, y))
